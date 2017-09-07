f303cf356db967ee6835c28485c3e3252541a1d1
Janitha Jayaweera <janitha_j@hotmail.com>
2017-09-07 16:02:28 -0700

    some changes

--- Week48/Source.cpp
+++ Week48/Source.cpp
@@ -6,21 +6,30 @@ int Solution::maximumSwap(int num) {
 		digits.push_back(num % 10);
 		num /= 10;
 	}
-	if (digits.size()==1) return numcopy;
+	if (digits.size() == 1) return numcopy;
 	else if (std::adjacent_find(digits.begin(), digits.end(), std::not_equal_to<int>()) == digits.end())
 	{
 		return numcopy;
 	}
-	else {//no restriction on number return sorted string
-		if (digits.at(1) < digits.at(0) ) { //MSD is smaller so swap
-			int swappednum = 0;
-			swappednum += digits.at(0) * 10;
-			swappednum += digits.at(1) * 1;
-			return swappednum;
+	else {
+		int swappednum = 0;
+		int templargest = 0;
+		std::max_element(digits.begin(), digits.end()));
+		for (int i = digits.size() - 1; i > 0; i--) {
+			
+			if (digits.at(i) < digits.at(i - 1)) { //MSD is smaller so swap works for 2 digits only
+				templargest = digits.at(i - 1);// *10 ^ i; //shift it 
+				swappednum += templargest * 10^i;
+				swappednum += digits.at(i) * 1;
+			}
+			else {
+				return numcopy;
+			}
 		}
-		else {
-			return numcopy;
-		}
+		return swappednum;
+		//return numcopy;
+
 	}
 	return 0;
+	//http://cs.indstate.edu/~spitla/abstract2.pdf
 }
